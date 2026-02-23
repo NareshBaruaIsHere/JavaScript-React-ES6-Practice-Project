841584a595c6daf79e3791157b9f3d7df57c8cb2
Naresh-Barua <nareshbarua72@gmail.com>
2026-02-23 20:04:40 +0600

    Initial commit

---
+++ day_1.js
@@ -0,0 +1,28 @@
+//? variable
+
+// let x = 2;
+// let y = 4;
+// let z = x+y;
+// console.log(z);
+
+// const x = 5;
+// const y = 6;
+// const z = x+y;
+// console.log(z);
+
+// let carName;
+// carName = "Mazda MX5";
+// console.log(carName);
+
+// const price1 = 5;
+// const price2 = 6;
+// let total = price1+price2;
+// console.log(total);
+
+//? Data type
+
+// const pi = 3.14;
+// let person = "Naresh Barua";
+// let answer = 'Yes I am!';
+// console.log(pi + "\n" + person + "\n" + answer);
+
