9007867c596f6adfb6849d1b632e2a1f5a7ba461
Naresh-Barua <nareshbarua72@gmail.com>
2026-02-24 23:06:26 +0600

    func can use many time

--- day_2/day_2.js
+++ day_2/day_2.js
@@ -6,8 +6,17 @@
 // let message = sayHello();
 // console.log(message);
 
-function sum(a,b){
-    return a*b;
-}
-let result = sum(20,10);
-console.log(result);
+// function sum(a,b){
+//     return a*b;
+// }
+// let result = sum(20,10);
+// console.log(result);
+
+// function add(a, b) {
+//   return a + b;
+// }
+// let sum1 = add(5, 5);
+// let sum2 = add(40, 40);
+// let result = [sum1, sum2];
+// console.log(result);
+
