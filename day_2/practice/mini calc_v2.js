c43912ae47b2c0561261d6b2b24a4afd306a9787
Naresh-Barua <nareshbarua72@gmail.com>
2026-02-24 23:22:32 +0600

    mini calc_v2

---
+++ day_2/practice/mini calc_v2.js
@@ -0,0 +1,10 @@
+const num1 = 10;
+const num2 = 5;
+
+console.log("Simple Calculator");
+console.log("First number:", num1);
+console.log("Second number:", num2);
+console.log("Addition (+):", num1 + num2);
+console.log("Subtraction (-):", num1 - num2);
+console.log("Multiplication (*):", num1 * num2);
+console.log("Division (/):", num1 / num2);
