b996e88e94bb1373b392c48c15807e77dec9586e
Naresh-Barua <nareshbarua72@gmail.com>
2026-02-26 21:37:12 +0600

    ternary

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -1,12 +1,24 @@
 //If conditional
 
-const hour = new Date().getHours();
-let greeting;
+// const hour = new Date().getHours();
+// let greeting;
 
-if (hour < 18) {
-  greeting = "Good Day";
-} else {
-  greeting = "Good Evening";
-}
+// if (hour < 18) {
+//   greeting = "Good Day";
+// } else {
+//   greeting = "Good Evening";
+// }
 
-console.log(greeting);
+// console.log(greeting);
+
+//? ternary
+// let age = 20;
+// let t = (age < 18) ? "Minor" : "Adult";
+// console.log(t);
+
+let price = 2;
+let isMember = false;
+let discount = isMember ? 0.2 : 0;
+let total = 10 - (price * discount);
+
+console.log(total);
