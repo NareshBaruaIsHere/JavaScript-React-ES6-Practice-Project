ccb31ee01356f020d3ca47731ba24120e1274dcf
Naresh-Barua <nareshbarua72@gmail.com>
2026-02-26 22:11:24 +0600

    logical

--- day_3/day_3.js
+++ day_3/day_3.js
@@ -25,8 +25,53 @@
 
 //? map()
 
-const nums = [1,2,3,4,5];
-const Dnum = nums.map(num => num * 2);
+// const nums = [1,2,3,4,5];
+// const Dnum = nums.map(num => num * 2);
 
-console.log('Original Num:', nums);
-console.log('Double Num:', Dnum);
+// console.log('Original Num:', nums);
+// console.log('Double Num:', Dnum);
+
+//! AND - && operator returns true if both expressions are true, otherwise false
+
+// let x = 6;
+// let y = 3;
+// let z = (x < 10 && y > 1)
+// console.log(z);
+
+//! OR -  || operator returns true if one or both expressions are true, otherwise false
+
+// let x = 6;
+// let y = -3;
+// let z = (x > 0 || y > 1)
+// console.log(z);
+
+//! NOT - NOT operator (!) returns true for false expressions and false for true expressions
+
+// let x = (5 == 8);
+// let y = !(5 == 8);
+// console.log(x,y);
+
+//! ?? operator returns the right operand when the left operand is nullish (null or undefined), otherwise it returns the left operand
+
+// let name = null;
+// let text = "missing";
+// let result = name ?? text;
+// console.log(result);
+
+//? Loops
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
