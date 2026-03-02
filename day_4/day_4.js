a49de06257879a538880e51bcbff85740d5d9ea2
Naresh-Barua <nareshbarua72@gmail.com>
2026-03-02 23:52:41 +0600

    while and do-while

--- day_4/day_4.js
+++ day_4/day_4.js
@@ -2,3 +2,17 @@
 for (let i = 1; i <= 5; i++) {
   console.log("Count:", i);
 }
+
+// While loop example
+let j = 1;
+while (j <= 3) {
+  console.log("While:", j);
+  j++;
+}
+
+// Do...while loop example
+let k = 1;
+do {
+  console.log("Do while:", k);
+  k++;
+} while (k <= 3);
